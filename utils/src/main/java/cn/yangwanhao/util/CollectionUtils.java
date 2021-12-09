4b84fc159af67e01a18933c479243dd77abf128f
杨万浩 <yangwanhao163@163.com>
2021-12-09 17:52:27 +0800

    CollectionUtils

---
+++ utils/src/main/java/cn/yangwanhao/util/CollectionUtils.java
@@ -0,0 +1,55 @@
+package cn.yangwanhao.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * CollectionUtils
+ *
+ * @author 杨万浩
+ * @version V3.0
+ * @since 2021/12/9 15:14
+ */
+public class CollectionUtils {
+
+    /**
+     * 校验Collection是否为空
+     *
+     * @param collection 校验Collection
+     * @return boolean true-为空，false-不为空
+     * @author jinyanan
+     */
+    public static boolean isEmpty(Collection collection) {
+        return (collection == null || collection.isEmpty());
+    }
+
+    /**
+     * 校验List是否不为空
+     *
+     * @param collection 校验Collection
+     * @return boolean true-不为空，false-为空
+     */
+    public static boolean isNotEmpty(Collection collection) {
+        return !isEmpty(collection);
+    }
+
+    /**
+     * 校验Map是否为空
+     *
+     * @param map map
+     * @return true-为空，false-不为空
+     */
+    public static boolean isEmpty(Map map) {
+        return map == null || map.isEmpty();
+    }
+
+    /**
+     * 校验Map是否不为空
+     *
+     * @param map map
+     * @return true-为空，false-不为空
+     */
+    public static boolean isNotEmpty(Map map) {
+        return !isEmpty(map);
+    }
+}
