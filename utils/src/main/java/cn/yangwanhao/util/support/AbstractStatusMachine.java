1f7139c61adeb3b313734dcd4bee7a38420b0fa6
杨万浩 <yangwanhao163@163.com>
2021-12-16 11:30:07 +0800

    状态机校验抽象类增加日志输出

--- utils/src/main/java/cn/yangwanhao/util/support/AbstractStatusMachine.java
+++ utils/src/main/java/cn/yangwanhao/util/support/AbstractStatusMachine.java
@@ -3,8 +3,10 @@ package cn.yangwanhao.util.support;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import cn.yangwanhao.util.po.StatusMachineRelation;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 状态机抽象工具类
@@ -13,6 +15,7 @@ import cn.yangwanhao.util.po.StatusMachineRelation;
  * @version V3.0
  * @since 2021/12/16 10:40
  */
+@Slf4j
 public abstract class AbstractStatusMachine {
 
     protected Map<Class, List<StatusMachineRelation>> map = new HashMap<>();
@@ -24,6 +27,11 @@ public abstract class AbstractStatusMachine {
         if (statusMachineRelations == null || statusMachineRelations.size() == 0) {
             throw new RuntimeException(clazz.getName() + "没有被初始化");
         }
+        List<String> allowedStatusList = statusMachineRelations.stream()
+            .filter(r -> r.getCurrentStatus().equals(currentStatus))
+            .map(StatusMachineRelation::getNextStatus)
+            .collect(Collectors.toList());
+        log.info("状态枚举[{}],当前状态:[{}],期望流转状态:[{}],允许流转状态:{}", clazz.getName(), currentStatus, nextStatus, allowedStatusList);
         for (StatusMachineRelation relation : statusMachineRelations) {
             if (relation.getCurrentStatus().equals(currentStatus) &&
                 relation.getNextStatus().equals(nextStatus)) {
